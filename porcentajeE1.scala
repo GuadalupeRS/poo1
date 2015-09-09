5f74600095b20a994313c6cab2516ce2dbb46a8e
Guadalupe <www.lupita13_12@hotmail.com>
2015-09-09 15:31:37 -0500

    termine actividad 2

---
+++ porcentajeE1.scala
@@ -0,0 +1,22 @@
+println("Cantidad 1: ");
+val cant1=readFloat();
+println(cant1);
+println("Cantidad 2: ");
+val cant2=readFloat();
+println(cant2);
+println("Cantidad 3: ");
+val cant3=readFloat();
+println(cant3);
+
+val cantotal=(cant1+cant2+cant3);
+println("Porcentaje 1: ");
+val porcentaje1= (cant1*100)/cantotal;
+println(porcentaje1);
+
+println("Porcentaje 2: ");
+val porcentaje2= (cant2*100)/cantotal;
+println(porcentaje2);
+
+println("Porcentaje 3: ");
+val porcentaje3= (cant3*100)/cantotal;
+println(porcentaje3);
