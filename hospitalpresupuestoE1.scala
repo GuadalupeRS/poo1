ae62d964711c5719cff3dac1fd24a3cb008e8a2f
Guadalupe <www.lupita13_12@hotmail.com>
2015-09-09 15:25:23 -0500

    termine ejercicio de presupuesto actividad 1

---
+++ hospitalpresupuestoE1.scala
@@ -0,0 +1,12 @@
+println("Presupuesto: ");
+val presupuesto=readFloat();
+println(presupuesto);
+
+val ginecologia=(presupuesto/100)*40;
+println("Ginecologia: " + ginecologia);
+
+val traumatologia=(presupuesto/100)*30;
+println("traumatologia: " + traumatologia);
+
+val pediatria=(presupuesto/100)*30;
+println("Pediatria: " + pediatria);
